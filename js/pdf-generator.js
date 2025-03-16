61cc6c0799070cf035bb858b193fe3305ed62121
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-03-16 15:08:17 -0300

    [DELETE] arquivos antigos

--- js/pdf-generator.js
+++ js/pdf-generator.js
@@ -1,4 +1,20 @@
-document.getElementById("btnGerarPDF").addEventListener("click", function () {
+function loadImage(file) {
+    return new Promise((resolve, reject) => {
+        if (!file) {
+            resolve(null); // Se não houver imagem, resolve imediatamente
+            return;
+        }
+
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            resolve(reader.result); // Retorna a imagem em Base64
+        };
+        reader.onerror = reject; // Rejeita se der erro
+    });
+}
+
+async function generatePDF() {
     const doc = new jsPDF();
 
     // Adiciona texto ao PDF
@@ -9,29 +25,26 @@ document.getElementById("btnGerarPDF").addEventListener("click", function () {
     let pontuacaoPorcento = document.getElementById("pontuacaoPorcento").value;
     let pontuacaoStatus = document.getElementById("pontuacaoStatus").value;
     let formName = document.getElementById("nome").value;
-    // let itenAttach = document.getElementById("file-multiple_page_titles");
     let itenComment = document.getElementById("text-multiple_page_titles").value;
 
-    // console.log(itenAttach);
+    let fileInput = document.getElementById("file-multiple_page_titles");
+    let file = fileInput.files[0]; // Pega o primeiro arquivo carregado
+    let imgData = await loadImage(file);
+
 
     doc.text(`Pontuação: ${pontuacaoFinal}`, 10, 20);
     doc.text(`Aprovação: ${pontuacaoPorcento}%`, 10, 30);
     doc.text(`Status: ${pontuacaoStatus}`, 10, 40);
     doc.text(`Nome: ${formName}`, 10, 50);
-    doc.text(`Nome: ${itenComment}`, 10, 60);
+    doc.text(`Comentario: ${itenComment}`, 10, 60);
 
-    let fileInput = document.getElementById("file-multiple_page_titles");
-    let file = fileInput.files[0]; // Pega o primeiro arquivo carregado
 
-    if (file) {
-        let reader = new FileReader();
-        reader.readAsDataURL(file); // Converte para Base64
-        reader.onload = function () {
-            let imgData = reader.result; // Base64 da imagem
-            doc.addImage(imgData, "PNG", 10, 70, 50, 50); // Adiciona ao PDF
-            doc.save("validacao.pdf");
-        };
+    if (imgData) {
+        doc.addImage(imgData, "jpeg", 10, 70, 50, 50); // Adiciona ao PDF
     }
 
     // Salva o PDF
-});
+    doc.save("validacao.pdf");
+}
+
+document.getElementById("btnGerarPDF").addEventListener("click", generatePDF);
